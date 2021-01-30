435f02761d906867b0783a960b253ca549516026
taisiia-filikova <filikova@gmail.com>
2021-01-30 22:28:17 +0200

    Revert "Обновляет index.js, удаляет лишнее из src"
    This reverts commit 30754149feec1fac9f49087dda2634c6880a1a33.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './redux/store';
-
 import './index.css';
 import App from './App';
-
+import reportWebVitals from './reportWebVitals';
 import 'modern-normalize/modern-normalize.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
